0ab60e5e5bd923086d8ffa38de74f758967ee1d0
sunkay <sunkay11@gmail.com>
2014-01-12 16:14:50 -0500

    streamline device edit and delete

--- client/views/devices/device_details.js
+++ client/views/devices/device_details.js
@@ -1,22 +1,3 @@
 Template.deviceDetails.events({
-	// Delete Device 
-	'click .delete': function(e){
-		e.preventDefault();
 
-		Logger.verbose("In delete event handler");
-
-		if(!Meteor.user()){
-			throwError("Access Denied... please login..");
-			Router.go('devicesList');
-			return;
-		}
-
-		if(confirm("Do you really want to delete this device?")){
-			var currentDeviceId = this._id;
-			Devices.remove(currentDeviceId, function(error){
-				throwError(error.reason);			
-			});
-			Router.go('devicesList');
-		}
-	}
 });
--- client/views/devices/device_item.js
+++ client/views/devices/device_item.js
@@ -1,3 +1,22 @@
-Template.deviceItem.helpers({
-	
+Template.deviceItem.events({
+		// Delete Device 
+	'click .delete': function(e){
+		e.preventDefault();
+
+		Logger.verbose("In delete event handler");
+
+		if(!Meteor.user()){
+			throwError("Access Denied... please login..");
+			Router.go('devicesList');
+			return;
+		}
+
+		if(confirm("Do you really want to delete this device?")){
+			var currentDeviceId = this._id;
+			Devices.remove(currentDeviceId, function(error){
+				throwError(error.reason);			
+			});
+			Router.go('devicesList');
+		}
+	}
 });
