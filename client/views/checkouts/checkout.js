3ce941211203e0cd6ca985dfecc5c1a7e553ef53
sunkay <sunkay11@gmail.com>
2014-01-18 15:10:38 -0500

    fully functional app.. onto bug fixing

--- client/views/checkouts/checkout.js
+++ client/views/checkouts/checkout.js
@@ -1,5 +1,15 @@
 Template.checkout.helpers({
 	checkoutDate: function () {
 		return new Date(this.checkoutDate).toString();
+	},
+	checkinDateTxt: function () {
+		return new Date(this.checkinDate).toString();
+	},
+	checkedinFlag: function(){
+		if(this.checkinDate == 0) 
+			return false;
+		else
+			return true;
 	}
+
 });
--- collections/checkouts.js
+++ collections/checkouts.js
@@ -19,6 +19,7 @@ Meteor.methods({
 			userId: user._id,
 			userName: user.username, 
 			checkoutDate: new Date().getTime(), 
+			checkinDate: 0,
 			comments: checkoutAttributes.comments
 		};
 
@@ -36,10 +37,18 @@ Meteor.methods({
 			throw new Meteor.Error(401, 'checkin: please login...');
 		}
 
-		Logger.info("updating checkin record: ", deviceId, "Method checkin");
+		Logger.info("updating checkin record: deviceId", deviceId, "Method checkin");
 
+		// update checkin date on the checkout record
+		checkoutId = Checkouts.findOne({deviceId:deviceId, checkinDate: 0})._id;
+		Logger.info("checkoutId", checkoutId, "Method Checkin");
+
+		var date = new Date().getTime();
+		Checkouts.update(checkoutId, {$set: {checkinDate: date}});
 
 		// mark the device as checked out
 		Devices.update(deviceId, {$set: {checkedout: 0}});
+
+
 	}
 });
