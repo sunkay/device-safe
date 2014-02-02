73eee8e02a54d99c305c472928e558e6a391a817
sunkay <sunkay11@gmail.com>
2014-02-02 11:42:13 -0500

    include moment atmosphere package for pretty date formatting

--- client/views/application/breadcrumbs/breadcrumbs.js
+++ client/views/application/breadcrumbs/breadcrumbs.js
@@ -36,3 +36,5 @@ Template.breadcrumbs.helpers({
 	}
 });
 
+
+
--- client/views/checkouts/checkout.js
+++ client/views/checkouts/checkout.js
@@ -1,9 +1,11 @@
 Template.checkout.helpers({
 	checkoutDate: function () {
-		return dateFormat(new Date(this.checkoutDate), "dddd, mmmm dS, yyyy, h:MM TT");
+		//return dateFormat(new Date(this.checkoutDate), "dddd, mmmm dS, yyyy, h:MM TT");
+		return moment(this.checkoutDate).fromNow();
 	},
 	checkinDateTxt: function () {
-		return dateFormat(new Date(this.checkinDate), "dddd, mmmm dS, yyyy, h:MM TT");
+		//return dateFormat(new Date(this.checkinDate), "dddd, mmmm dS, yyyy, h:MM TT");
+		return moment(this.checkinDate).fromNow();
 	},
 	checkedinFlag: function(){
 		if(this.checkinDate == 0) 
