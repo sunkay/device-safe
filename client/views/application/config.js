22fbf82a8436370240658469dcbf6b2cbce4e396
sunkay <sunkay@yahoo.com>
2014-04-29 08:52:00 -0400

    integrate accounts-entry and profile packages for single page login views

--- client/views/application/config.js
+++ client/views/application/config.js
@@ -1,3 +1,14 @@
 Accounts.ui.config({
 	passwordSignupFields: 'USERNAME_ONLY' //  One of 'USERNAME_AND_EMAIL', 'USERNAME_AND_OPTIONAL_EMAIL', 'USERNAME_ONLY', or 'EMAIL_ONLY' (default).
 });
+
+AccountsEntry.config({
+	//logo: 'logo.png',
+    //privacyUrl: '/privacy-policy',
+    //termsUrl: '/terms-of-use',
+    homeRoute: '/',
+    dashboardRoute: '/',
+    profileRoute: 'profile',
+    //showSignupCode: true,
+	passwordSignupFields: 'USERNAME_ONLY' //  One of 'USERNAME_AND_EMAIL', 'USERNAME_AND_OPTIONAL_EMAIL', 'USERNAME_ONLY', or 'EMAIL_ONLY' (default).
+});
