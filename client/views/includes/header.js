f7a00b84f10e6af28156ff3b1fa4cde514c6a183
sunkay <sunkay11@gmail.com>
2014-09-02 22:24:54 -0400

    still fixing navigation quirks, used Meteor.setInterval. Issue of slide Menu wobbling still exists

--- client/views/includes/header.js
+++ client/views/includes/header.js
@@ -1,31 +1,11 @@
-/*
-Template.header.rendered = function () {
-  $(document).ready(function () {
-      $('.slideout-menu-toggle').on('click', function(event){
-        event.preventDefault();
-        event.stopPropagation();
+Template.header.helpers({
+  navbarToggleCollapse: function(){
+     return Session.get('navbarToggle');
+  }
+});
 
-        // create menu variables
-        var slideoutMenu = $('.slideout-menu');
-        var slideoutMenuWidth = $('.slideout-menu').width();
-        
-        // toggle open class
-        slideoutMenu.toggleClass("open");
-        
-        // slide menu Open
-        if (slideoutMenu.hasClass("open")) { 
-          slideoutMenu.animate({left: "0px"});
- 
-          // create a one-time event to close when a user clicks anywhere outside
-          $(document).one('touchstart click', function(){
-            slideoutMenu.toggleClass("open");
-            slideoutMenu.animate({left: -slideoutMenuWidth}, 250); 
-          });
-        } else {
-          // slide menu close
-          slideoutMenu.animate({left: -slideoutMenuWidth}, 250);  
-        }
-      });
-  });
+Template.header.rendered = function () {
+   Meteor.setInterval(function(){
+    Session.set('navbarToggle', $('#btnRespNav').is(':visible'));
+   }, 3000);
 };
-*/
